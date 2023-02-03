0a9426d1441ae951cec37734b3c5167fb9314fea
saadyousafi87 <saad.yousafi87@gmail.com>
2023-02-03 14:56:38 -0500

    implemented putDb() and getDb()

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,42 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('putDb implemented');
+
+  // Create a connection to the database
+  const jateDb = await openDB('jate', 1);
+
+  // Creating new transaction
+  const transac = dateDb.transaction('jate', 'readwrite');
+
+  // Opening the object store
+  const store = transac.objectStore('jate');
+
+  // Adding to the store
+  const request = store.add({ content});
+
+  // Getting comfirmation
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('getDb implemented');
+
+  const jateDb = await openDB('jate', 1);
+
+  const transac = jateDb.transaction('jate', 'readonly');
+
+  const store = transac.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
